7298fb76f3ddae4adcf3237e1cbb332bb80a35f6
Zack Yang <zack9433@gmail.com>
2016-07-20 14:57:12 +0800

    fix(refactor): refactor component

--- app/app.js
+++ app/app.js
@@ -1,10 +1,14 @@
 import 'toastr.scss';
 import './app.scss';
 import angular from 'angular';
-import exception from './component';
+import {sjException} from './component';
 
-angular.module('webapp', [exception])
+angular.module('webapp', [sjException])
 .run((exception) => {
   exception.catcher('[Exception catcher]:')({data: {status: 500, message: 'Internal error'}});
   exception.catcher('Input format error!')('because ip format error.');
+});
+
+angular.element(document).ready(() => {
+  angular.bootstrap(document.body, ['webapp']);
 });
