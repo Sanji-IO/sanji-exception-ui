4da1aa694452cb81fba161ddf5732f001c4792c7
Zack Yang <zack9433@gmail.com>
2016-12-19 16:23:25 +0800

    fix(build): upgrade webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const ProgressBarPlugin = require('progress-bar-webpack-plugin');
 const LodashModuleReplacementPlugin = require('lodash-webpack-plugin');
 const NODE_ENV = process.env.NODE_ENV;
 const nodeRoot = path.join(__dirname, 'node_modules');
-const appRoot = path.join(__dirname, 'app');
+const appRoot = path.join(__dirname, 'src');
 const config = {
   context: appRoot,
   output: {
@@ -19,9 +19,9 @@ const config = {
   },
   module: {
     rules: [
-      { test: /\.js$/, loader: 'eslint-loader', exclude: /node_modules/, enforce: 'pre' },
-      {test: /\.js$/, loader: 'babel-loader?cacheDirectory', exclude: /(node_modules)/},
-      { test: /\.html$/, loader: 'ng-cache-loader?prefix=[dir]/[dir]', exclude: [/node_modules/, path.join(__dirname, '/app/index.html')] }
+      { test: /\.js$/, use: 'eslint-loader', exclude: /node_modules/, enforce: 'pre' },
+      { test: /\.js$/, use: 'babel-loader?cacheDirectory', exclude: /(node_modules)/ },
+      { test: /\.html$/, use: 'ng-cache-loader?prefix=[dir]/[dir]', exclude: [/node_modules/, path.join(__dirname, '/src/index.html')] }
     ]
   },
   plugins: [
